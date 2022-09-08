fb50c9c862c1cb651ca38cdf2a9f334117a4fcc7
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2022-09-08 08:49:32 -0700

    Add Contemporary Movement Review

--- public/js/front.js
+++ public/js/front.js
@@ -21,8 +21,6 @@ $(".fix-review-button").click(function() {
   review = review.replaceAll("\"", "\\\"");
   review = review.replaceAll("\n", "\\n");
   let tracklist = JSON.parse($(".tracklist-text").val())
-  console.log(tracklist[25].replaceAll("\"", "\\\""))
-  console.log(review.includes(tracklist[25].replaceAll("\"", "\\\"")))
   for (var i = 0; i < tracklist.length; i++) {
     if(review.includes("\\\"" + tracklist[i].replaceAll("\"", "\\\"") + "\\\"")){
       review = review.replace(
@@ -43,6 +41,10 @@ $(".fix-tracklist-button").click(function() {
     }
   }
   for (var i = 0; i < fixed_tracklist.length; i++) {
+    fixed_tracklist[i] = fixed_tracklist[i].replaceAll("’", "'");
+    fixed_tracklist[i] = fixed_tracklist[i].replaceAll("‘", "'");
+    fixed_tracklist[i] = fixed_tracklist[i].replaceAll("”", "\"");
+    fixed_tracklist[i] = fixed_tracklist[i].replaceAll("“", "\"");
     fixed_tracklist[i] = fixed_tracklist[i].replaceAll("\\\"", "\"");
   }
   $(".tracklist-text").val(JSON.stringify(fixed_tracklist));
