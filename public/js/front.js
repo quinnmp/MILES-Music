22ff6f4cd2ab4097166b59a7ca5fa87c01b0d6b4
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2022-09-09 12:47:59 -0700

    Fix footer positioning and pause/play symbols

--- public/js/front.js
+++ public/js/front.js
@@ -27,7 +27,15 @@ function onPlayerReady(event) {
 }
 
 function onPlayerStateChange(event) {
-  // console.log(player.getVideoEmbedCode());
+  if(event.data === 1) {
+    $(".pause-button").css("display", "initial")
+  } else if(event.data === 2 || event.data === 0) {
+    $(".timestamp").children().attr("class", "play");
+    $(".pause-button").css("display", "none")
+  }
+  $(".play").html("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-play-fill\" viewBox=\"0 0 16 16\"><path d=\"m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z\"/></svg>")
+  $(".pause").html("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pause-fill\" viewBox=\"0 0 16 16\"><path d=\"M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z\"/></svg>")
+  console.log(event.data)
 }
 
 if (page !== "") {
@@ -49,6 +57,10 @@ $(".track-wrapper-generation-button").click(function() {
   player.playVideo();
 })
 
+$(".pause-button").click(function() {
+  player.pauseVideo();
+})
+
 $(".timestamp").click(function() {
   if($(this).children().attr("class") === "play") {
     let duration = parseInt($(this).text().substring(0, $(this).text().indexOf(":")), 10) * 60 + parseInt($(this).text().substring($(this).text().indexOf(":") + 1, $(this).text().length), 10)
@@ -63,8 +75,6 @@ $(".timestamp").click(function() {
     player.pauseVideo();
     $(this).children().attr("class", "play");
   }
-  $(".play").html("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-play-fill\" viewBox=\"0 0 16 16\"><path d=\"m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z\"/></svg>")
-  $(".pause").html("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pause-fill\" viewBox=\"0 0 16 16\"><path d=\"M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z\"/></svg>")
 })
 
 $(".fix-review-button").click(function() {
